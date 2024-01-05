5d4056b0130cd7447777fafacaf6cda0434a1e50
Gil Balsiger <gil.balsiger@gmail.com>
2024-01-05 17:55:20 +0100

    chore: add postcss-preset-env

--- example/postcss.config.js
+++ example/postcss.config.js
@@ -2,6 +2,9 @@ export default {
   plugins: {
     "postcss-import": {},
     "tailwindcss/nesting": "postcss-nesting",
+    "postcss-preset-env": {
+      features: { "nesting-rules": false },
+    },
     tailwindcss: {},
     autoprefixer: {},
   },
--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,9 @@ export default {
   plugins: {
     "postcss-import": {},
     "postcss-nesting": {},
+    "postcss-preset-env": {
+      features: { "nesting-rules": false },
+    },
     autoprefixer: {},
     cssnano: {},
   },
