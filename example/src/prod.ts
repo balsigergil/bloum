4e0ba9758fb1c42eb597f69269fba3396367d841
Gil Balsiger <gil.balsiger@gmail.com>
2024-01-16 20:49:25 +0100

    refactor: update tailwind example

---
+++ example/src/prod.ts
@@ -0,0 +1,2 @@
+import "bloum";
+import "./example.css";
--- example/src/tailwind.ts
+++ example/src/tailwind.ts
@@ -3,5 +3,4 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "../../src/style.css";
 
 import "./tailwind.css";
-import "./select.css";
 import "../../src";
