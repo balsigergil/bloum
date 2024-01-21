0a0f0918535f1c1dde50a93d2522e17cee23e7a2
Gil Balsiger <gil.balsiger@gmail.com>
2024-01-21 22:32:15 +0100

    fix(closeButton): fire click event when Enter or space is pressed

--- src/close/CloseButton.ts
+++ src/close/CloseButton.ts
@@ -14,5 +14,12 @@ export class CloseButton extends HTMLElement {
     this.classList.add("bl-close-button");
     this.role = "button";
     this.innerHTML = CLOSE_ICON;
+    this.tabIndex = 0;
+    this.addEventListener("keydown", (e) => {
+      if (e.key === "Enter" || e.key === " ") {
+        this.dispatchEvent(new Event("click"));
+        e.preventDefault();
+      }
+    });
   }
 }
