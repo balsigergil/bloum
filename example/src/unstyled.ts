d0550cf3976a62b6de9b2d9e60b3601a7ba50ae9
Gil Balsiger <gil.balsiger@gmail.com>
2024-01-28 12:37:50 +0100

    refactor(close-btn): add CSS variables

--- example/src/unstyled.ts
+++ example/src/unstyled.ts
@@ -9,3 +9,20 @@ document.querySelectorAll("form").forEach((f) =>
     console.log(formData);
   }),
 );
+
+document.getElementById("light")?.addEventListener("click", () => {
+  document.documentElement.classList.remove("dark");
+  document.documentElement.classList.add("light");
+  document
+    .querySelector("meta[name='color-scheme']")
+    ?.setAttribute("content", "light");
+});
+
+document.getElementById("dark")?.addEventListener("click", () => {
+  document.documentElement.classList.remove("light");
+  document.documentElement.classList.add("dark");
+
+  document
+    .querySelector("meta[name='color-scheme']")
+    ?.setAttribute("content", "dark");
+});
