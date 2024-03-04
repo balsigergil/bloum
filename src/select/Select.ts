60f475e32fdc6bd6a609a9a707924a6c42918e64
Gil Balsiger <gil.balsiger@gmail.com>
2024-03-04 16:54:10 +0100

    refactor(select): update events

--- src/select/Select.ts
+++ src/select/Select.ts
@@ -14,7 +14,7 @@ export class Select extends HTMLElement {
   static NAME = "bl-select";
 
   #select!: HTMLSelectElement;
-  #input!: HTMLInputElement;
+  #input: HTMLInputElement | undefined = undefined;
   #menu!: HTMLDivElement;
   #optionWrapper!: HTMLDivElement;
   #text!: HTMLDivElement;
@@ -39,8 +39,8 @@ export class Select extends HTMLElement {
 
   connectedCallback() {
     this.classList.add(Select.NAME);
-
     this.tabIndex = 0;
+
     this.addEventListener("click", (e) => {
       e.preventDefault();
       setTimeout(() => {
@@ -53,9 +53,47 @@ export class Select extends HTMLElement {
     });
 
     this.addEventListener("keydown", (e) => {
-      if (e.key === "ArrowDown" || e.key === "ArrowUp" || e.key === "Enter") {
-        this.openMenu();
+      if (e.key === "Escape") {
+        this.closeMenu();
         e.preventDefault();
+        e.stopPropagation();
+      }
+      if (e.key === "ArrowDown") {
+        e.stopPropagation();
+        e.preventDefault();
+        this.#focusNext();
+      }
+      if (e.key === "ArrowUp") {
+        e.preventDefault();
+        e.stopPropagation();
+        this.#focusPrevious();
+      }
+      if (e.key === "Tab") {
+        this.closeMenu(false);
+      }
+      if (
+        e.key === "Backspace" &&
+        this.#input &&
+        this.#input.value === "" &&
+        this.#multiple
+      ) {
+        e.preventDefault();
+        e.stopPropagation();
+        if (this.#optionFlags.length > 0) {
+          this.#deselectLast();
+        }
+      }
+      if (e.key === "Enter") {
+        e.preventDefault();
+        e.stopPropagation();
+        if (this.isMenuOpen) {
+          if (this.#focusedItemIndex !== null) {
+            this.#setSelected(this.#focusedItemIndex);
+          }
+          this.closeMenu();
+        } else {
+          this.openMenu();
+        }
       }
     });
 
@@ -75,62 +113,26 @@ export class Select extends HTMLElement {
     this.#text = document.createElement("div");
     this.#text.classList.add("bl-select-text");
 
-    this.#input = document.createElement("input");
-    this.#input.setAttribute("type", "search");
-    this.#input.setAttribute("autocomplete", "off");
-    this.#input.setAttribute("spellcheck", "false");
-    this.#input.setAttribute("role", "combobox");
-    this.#input.setAttribute("aria-autocomplete", "list");
-    this.#input.setAttribute("aria-expanded", "false");
-    if (!this.#searchable || this.#disabled) {
-      this.#input.readOnly = true;
-    }
-    this.#input.classList.add("bl-select-search");
-    this.#input.addEventListener("click", (e) => {
-      e.stopPropagation();
-    });
-    this.#input.addEventListener("input", () => this.#onInput());
-    this.#input.addEventListener("keydown", (e) => {
-      if (e.key === "Escape") {
-        this.closeMenu();
-        e.preventDefault();
-        e.stopPropagation();
-      }
-      if (e.key === "ArrowDown") {
-        e.stopPropagation();
-        e.preventDefault();
-        this.#focusNext();
-      }
-      if (e.key === "ArrowUp") {
-        e.preventDefault();
-        e.stopPropagation();
-        this.#focusPrevious();
-      }
-      if (e.key === "Backspace" && this.#input.value === "" && this.#multiple) {
-        e.preventDefault();
-        e.stopPropagation();
-        if (this.#optionFlags.length > 0) {
-          this.#deselectLast();
-        }
-      }
-      if (e.key === "Tab") {
-        e.preventDefault();
-        e.stopPropagation();
-      }
-      if (e.key === "Enter") {
-        e.preventDefault();
-        e.stopPropagation();
-        if (this.#focusedItemIndex !== null && this.isMenuOpen) {
-          this.#setSelected(this.#focusedItemIndex);
-        }
-        this.closeMenu();
-      }
-    });
-
     this.#menu = document.createElement("div");
     this.#menu.classList.add("bl-select-menu");
 
-    this.#menu.append(this.#input);
+    if (this.#searchable && !this.#disabled) {
+      this.#input = document.createElement("input");
+      this.#input.setAttribute("type", "search");
+      this.#input.setAttribute("autocomplete", "off");
+      this.#input.setAttribute("spellcheck", "false");
+      this.#input.setAttribute("role", "combobox");
+      this.#input.setAttribute("aria-autocomplete", "list");
+      this.#input.setAttribute("aria-expanded", "false");
+      this.#input.classList.add("bl-select-search");
+      this.#input.addEventListener("click", (e) => {
+        // Prevent the click to close the menu
+        e.stopPropagation();
+      });
+      this.#input.addEventListener("input", () => this.#onInput());
+      this.#menu.append(this.#input);
+    }
+
     this.#optionWrapper = document.createElement("div");
     this.#optionWrapper.classList.add("bl-select-options");
     this.#optionWrapper.setAttribute("role", "listbox");
@@ -148,15 +150,18 @@ export class Select extends HTMLElement {
     indicator.classList.add("bl-select-indicator");
     indicator.innerHTML = `<svg height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false"><path fill="currentColor" d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path></svg>`;
 
-    const clearButton = new CloseButton();
-    clearButton.classList.add("bl-select-clear-button");
-    clearButton.addEventListener("click", (e) => {
-      e.preventDefault();
-      e.stopPropagation();
-      this.clear();
-    });
+    if (this.#clearable) {
+      const clearButton = new CloseButton();
+      clearButton.classList.add("bl-select-clear-btn");
+      clearButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        this.clear();
+      });
+      indicators.append(clearButton);
+    }
 
-    indicators.append(clearButton, indicator);
+    indicators.append(indicator);
 
     const selected = this.getAttribute("selected") || "";
     const selectedIndex = this.#getOptionByValue(selected);
@@ -237,6 +242,7 @@ export class Select extends HTMLElement {
     for (let i = 0; i < options.length; i++) {
       const option = options[i];
       const isSelected = this.#optionFlags[i];
+
       if (isSelected) {
         const clone = option.cloneNode(true) as HTMLElement;
         clone.classList.remove("bl-option", "focus", "selected", "filtered");
@@ -244,6 +250,7 @@ export class Select extends HTMLElement {
         const textWrapper = document.createElement("div");
         textWrapper.classList.add("bl-select-text-wrapper");
         textWrapper.append(clone);
+
         if (this.#multiple) {
           const closeButton = new CloseButton();
           closeButton.addEventListener("click", (e) => {
@@ -259,7 +266,13 @@ export class Select extends HTMLElement {
         option.classList.remove("selected");
         option.setAttribute("aria-selected", "false");
       }
-      (this.#select.childNodes[i] as HTMLOptionElement).selected = isSelected;
+
+      const selectedOption = this.#select.childNodes[i] as
+        | HTMLOptionElement
+        | undefined;
+      if (selectedOption) {
+        selectedOption.selected = isSelected;
+      }
 
       if (i === this.#focusedItemIndex) {
         option.classList.add("focus");
@@ -285,19 +298,15 @@ export class Select extends HTMLElement {
       this.#text.innerHTML = `<div class="bl-select-placeholder">${this.#placeholder}</div>`;
     }
 
-    this.#menu.querySelectorAll(".no-result").forEach((n) => n.remove());
+    this.#optionWrapper
+      .querySelectorAll(".no-result")
+      .forEach((n) => n.remove());
     if (count === 0) {
       const noResult = document.createElement("div");
-      noResult.classList.add("bl-select-menu-item");
+      noResult.classList.add("bl-option");
       noResult.classList.add("no-result");
       noResult.textContent = this.#noResultsText;
-      this.#menu.append(noResult);
-    }
-
-    if (this.#selectedCount > 0 && this.#clearable) {
-      this.querySelector(".bl-select-clear-button")?.classList.add("show");
-    } else {
-      this.querySelector(".bl-select-clear-button")?.classList.remove("show");
+      this.#optionWrapper.append(noResult);
     }
   }
 
@@ -305,9 +314,14 @@ export class Select extends HTMLElement {
     if (this.#disabled) {
       return;
     }
+    if (this.isMenuOpen) {
+      return;
+    }
     this.classList.add("open");
-    this.#input.focus();
-    this.#input.setAttribute("aria-expanded", "true");
+    if (this.#input) {
+      this.#input.focus();
+      this.#input.setAttribute("aria-expanded", "true");
+    }
     this.#setFocused(this.#firstSelectedIndex() || 0);
   }
 
@@ -315,8 +329,10 @@ export class Select extends HTMLElement {
     if (this.classList.contains("open")) {
       this.classList.remove("open");
       this.#text.classList.remove("filtered");
-      this.#input.value = "";
-      this.#input.setAttribute("aria-expanded", "false");
+      if (this.#input) {
+        this.#input.value = "";
+        this.#input.setAttribute("aria-expanded", "false");
+      }
       this.#search = "";
       if (focus) {
         this.focus();
@@ -331,6 +347,9 @@ export class Select extends HTMLElement {
 
   #onInput() {
     this.openMenu();
+    if (this.#input === undefined) {
+      return;
+    }
     this.#search = this.#input.value || "";
     const options = this.filteredOptions;
     if (options.length > 0) {
