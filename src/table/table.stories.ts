5ac897e110aba4b9db2e96b5af21e26f3334f10f
Gil Balsiger <gil.balsiger@gmail.com>
2024-12-08 16:57:44 +0100

    feat: table OK

---
+++ src/table/table.stories.ts
@@ -0,0 +1,116 @@
+import type { Meta, StoryObj } from "@storybook/html";
+
+type TableArgs = {
+  striped: boolean;
+  hover: boolean;
+};
+
+const meta: Meta<TableArgs> = {
+  title: "Table",
+  render: (args) => {
+    const table = document.createElement("table");
+    table.classList.add("table");
+
+    if (args.striped) {
+      table.classList.add("table-striped");
+    }
+
+    if (args.hover) {
+      table.classList.add("table-hover");
+    }
+
+    table.innerHTML = `
+      <thead>
+        <tr>
+          <th>
+            <input type="checkbox" aria-label="Select user" />
+          </th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Role</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <input type="checkbox" aria-label="Select user" />
+          </td>
+          <td>Facteur Hyacinthe</td>
+          <td>john.doe@example.com</td>
+          <td>Admin</td>
+          <td>
+            <button class="btn btn-icon btn-xs btn-ghost" aria-label="Edit user">
+              <i class="fas fa-pen"></i>
+            </button>
+            <button class="btn btn-icon btn-xs btn-danger" aria-label="Delete user">
+              <i class="fas fa-trash"></i>
+            </button>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="checkbox" aria-label="Select user" />
+          </td>
+          <td>Albert le Vert</td>
+          <td>john.doe@example.com</td>
+          <td>Admin</td>
+          <td>
+            <button class="btn btn-icon btn-xs btn-ghost" aria-label="Edit user">
+              <i class="fas fa-pen"></i>
+            </button>
+            <button class="btn btn-icon btn-xs btn-danger" aria-label="Delete user">
+              <i class="fas fa-trash"></i>
+            </button>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <input type="checkbox" aria-label="Select user" />
+          </td>
+          <td>Mademoiselle Cassis</td>
+          <td>john.doe@example.com</td>
+          <td>Admin</td>
+          <td>
+            <button class="btn btn-icon btn-xs btn-ghost" aria-label="Edit user">
+              <i class="fas fa-pen"></i>
+            </button>
+            <button class="btn btn-icon btn-xs btn-danger" aria-label="Delete user">
+              <i class="fas fa-trash"></i>
+            </button>
+          </td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <td colspan="5">Total: 1 user</td>
+        </tr>
+      </tfoot>
+      `;
+    return table;
+  },
+};
+
+export default meta;
+type Story = StoryObj<TableArgs>;
+
+export const Table: Story = {
+  args: {
+    striped: false,
+    hover: false,
+  },
+};
+
+export const TableStriped: Story = {
+  args: {
+    ...Table.args,
+    striped: true,
+  },
+};
+
+export const TableHover: Story = {
+  args: {
+    ...Table.args,
+    hover: true,
+  },
+};
