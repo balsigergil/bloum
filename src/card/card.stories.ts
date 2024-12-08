3845e0eaf5e831ffe6beb7a1f4391f2ba49aa56e
Gil Balsiger <gil.balsiger@gmail.com>
2024-12-08 17:25:54 +0100

    feat: table in card

--- src/card/card.stories.ts
+++ src/card/card.stories.ts
@@ -28,3 +28,91 @@ export default meta;
 type Story = StoryObj;
 
 export const Card: Story = {};
+
+export const CardWithTable: Story = {
+  render: () => {
+    return `
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">List of users</h3>
+          <div class="card-actions">
+            <button class="btn btn-icon btn-ghost" aria-label="Add user">
+              <i class="fas fa-plus"></i>
+            </button>
+            <button class="btn btn-icon btn-ghost" aria-label="Settings">
+              <i class="fas fa-cog"></i>
+            </button>
+          </div>
+        </div>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>
+                <input type="checkbox" aria-label="Select user" />
+              </th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <input type="checkbox" aria-label="Select user" />
+              </td>
+              <td>Facteur Hyacinthe</td>
+              <td>john.doe@example.com</td>
+              <td>Admin</td>
+              <td>
+                <button class="btn btn-icon btn-xs btn-ghost" aria-label="Edit user">
+                  <i class="fas fa-pen"></i>
+                </button>
+                <button class="btn btn-icon btn-xs btn-danger" aria-label="Delete user">
+                  <i class="fas fa-trash"></i>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <input type="checkbox" aria-label="Select user" />
+              </td>
+              <td>Albert le Vert</td>
+              <td>john.doe@example.com</td>
+              <td>Admin</td>
+              <td>
+                <button class="btn btn-icon btn-xs btn-ghost" aria-label="Edit user">
+                  <i class="fas fa-pen"></i>
+                </button>
+                <button class="btn btn-icon btn-xs btn-danger" aria-label="Delete user">
+                  <i class="fas fa-trash"></i>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <input type="checkbox" aria-label="Select user" />
+              </td>
+              <td>Mademoiselle Cassis</td>
+              <td>john.doe@example.com</td>
+              <td>Admin</td>
+              <td>
+                <button class="btn btn-icon btn-xs btn-ghost" aria-label="Edit user">
+                  <i class="fas fa-pen"></i>
+                </button>
+                <button class="btn btn-icon btn-xs btn-danger" aria-label="Delete user">
+                  <i class="fas fa-trash"></i>
+                </button>
+              </td>
+            </tr>
+          </tbody>
+<!--          <tfoot>-->
+<!--            <tr>-->
+<!--              <td colspan="5">Total: 1 user</td>-->
+<!--            </tr>-->
+<!--          </tfoot>-->
+        </table>
+      </div>
+      `;
+  },
+};
