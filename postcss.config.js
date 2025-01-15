f10e9c6974ab456903c1d35e815d8876e83d53da
Gil Balsiger <gil.balsiger@gmail.com>
2025-01-15 21:28:21 +0100

    chore: update postcss

--- postcss.config.js
+++ postcss.config.js
@@ -2,11 +2,12 @@ export default {
   plugins: {
     "postcss-import": {},
     "postcss-nesting": {},
-    "postcss-preset-env": {
-      features: { "nesting-rules": false },
-    },
     tailwindcss: {},
-    autoprefixer: {},
-    // cssnano: {},
+    "postcss-preset-env": {
+      features: {
+        "nesting-rules": false,
+      },
+    },
+    cssnano: {},
   },
 };
