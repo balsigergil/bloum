8ffb5c27cd8bcdae2ca4c856136ecad584842bcb
Gil Balsiger <gil.balsiger@gmail.com>
2025-01-25 11:32:06 +0100

    chore: update storybook, update types generation

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,23 @@
+import { dirname, join } from "path";
 import type { StorybookConfig } from "@storybook/html-vite";
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-essentials",
-    "@storybook/addon-links",
-    "@storybook/addon-interactions",
-    "@storybook/addon-themes",
-    "@storybook/addon-a11y",
-    "@storybook/addon-storysource",
-    "@storybook/addon-docs",
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-themes"),
+    getAbsolutePath("@storybook/addon-a11y"),
+    getAbsolutePath("@storybook/addon-storysource"),
+    getAbsolutePath("@storybook/addon-docs"),
   ],
   framework: {
-    name: "@storybook/html-vite",
+    name: getAbsolutePath("@storybook/html-vite"),
     options: {},
   },
 };
