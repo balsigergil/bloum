9bf768803fe8fafdcfaa0cf580b1d945a5e013c6
Gil Balsiger <gil.balsiger@gmail.com>
2025-01-25 12:31:53 +0100

    chore: removed docs, update deps

--- docs/README.md
+++
@@ -1,5 +0,0 @@
-# Bloum documentation website
-
-This repository contains the source code for the [Bloum website](https://www.bloum.dev).
-
-Made with [Docusaurus](https://docusaurus.io/).
--- scripts/copy.js
+++
@@ -1,9 +0,0 @@
-// Copy all javascript and css from dist to docs/static/dist
-
-import { copySync } from "fs-extra/esm";
-import path from "node:path";
-
-const src = path.resolve("dist");
-const dest = path.resolve("docs/static/dist");
-
-copySync(src, dest);
