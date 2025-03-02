ae6a1eb4b231339f131e21f741a8e7f4b7572ce1
Gil Balsiger <gil.balsiger@gmail.com>
2025-03-02 20:36:22 +0100

    feat(menu): add danger menu item

--- src/components/menu/menu.stories.ts
+++ src/components/menu/menu.stories.ts
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from "@storybook/html";
 
 const meta: Meta = {
   title: "Components/Menu",
+  tags: ["!autodocs"],
 };
 
 export default meta;
@@ -18,6 +19,40 @@ export const Menu: Story = {
     <button class="menu-item">Save</button>
     <div class="menu-divider"></div>
     <button class="menu-item">Close</button>
+  </div>
+</div>
+`;
+  },
+};
+
+export const MenuWithIcons: Story = {
+  render: () => {
+    return `
+<div class="dropdown">
+  <button class="btn" data-menu="#menu">Click to open the menu</button>
+  <div class="menu" id="menu">
+    <a href="#" class="menu-item">New File</a>
+    <a href="#" class="menu-item">Open File...</a>
+    <button class="menu-item">Save</button>
+    <div class="menu-divider"></div>
+    <button class="menu-item">Close</button>
+  </div>
+</div>
+`;
+  },
+};
+
+export const MenuWithDanger: Story = {
+  render: () => {
+    return `
+<div class="dropdown">
+  <button class="btn" data-menu="#menu">Click to open the menu</button>
+  <div class="menu" id="menu">
+    <a href="#" class="menu-item">New File</a>
+    <a href="#" class="menu-item">Open File...</a>
+    <button class="menu-item">Save</button>
+    <div class="menu-divider"></div>
+    <button class="menu-item menu-item-danger">Delete</button>
   </div>
 </div>
 `;
