26eedb94adcd80f377fb80aaf64ee785751066ad
Gil Balsiger <gil.balsiger@gmail.com>
2025-03-02 17:24:04 +0100

    refactor: tweak colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.stories.ts"],
+  content: ["./src/**/*.stories.ts", "./src/**/*.mdx", "./src/**/*.tsx"],
   theme: {
     extend: {},
   },
