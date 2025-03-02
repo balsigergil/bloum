af6ff0ca63b5899e5955e27cdb002ba3b50a6ffa
Gil Balsiger <gil.balsiger@gmail.com>
2025-03-02 20:52:21 +0100

    feat(menu): add support for icons and checkboxes

--- src/components/menu/menu.stories.ts
+++ src/components/menu/menu.stories.ts
@@ -29,13 +29,13 @@ export const MenuWithIcons: Story = {
   render: () => {
     return `
 <div class="dropdown">
-  <button class="btn" data-menu="#menu">Click to open the menu</button>
+  <button class="btn" data-menu="#menu"><i class="fas fa-bars"></i>Click to open the menu</button>
   <div class="menu" id="menu">
-    <a href="#" class="menu-item">New File</a>
-    <a href="#" class="menu-item">Open File...</a>
-    <button class="menu-item">Save</button>
+    <a href="#" class="menu-item"><i class="fas fa-file fa-fw"></i> New File</a>
+    <a href="#" class="menu-item"><i class="fas fa-folder-open fa-fw"></i> Open...</a>
+    <button class="menu-item"><i class="fas fa-floppy-disk fa-fw"></i> Save</button>
     <div class="menu-divider"></div>
-    <button class="menu-item">Close</button>
+    <button class="menu-item"><i class="fas fa-close fa-fw"></i> Close</button>
   </div>
 </div>
 `;
@@ -53,6 +53,23 @@ export const MenuWithDanger: Story = {
     <button class="menu-item">Save</button>
     <div class="menu-divider"></div>
     <button class="menu-item menu-item-danger">Delete</button>
+  </div>
+</div>
+`;
+  },
+};
+
+export const MenuWithCheckbox: Story = {
+  render: () => {
+    return `
+<div class="dropdown">
+  <button class="btn" data-menu="#menu"><i class="fas fa-cog"></i>Features</button>
+  <div class="menu" id="menu">
+    <label class="menu-item"><input type="checkbox" class="checkbox">Feature 1</label>
+    <label class="menu-item"><input type="checkbox" class="checkbox">Feature 2</label>
+    <div class="menu-divider"></div>
+    <label class="menu-item"><input type="checkbox" class="switch">Feature 3</label>
+    <label class="menu-item"><input type="checkbox" class="switch">Feature 4</label>
   </div>
 </div>
 `;
