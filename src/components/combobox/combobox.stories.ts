20a7b8ec7ecf215dfac047ac796aa9993f9a66b7
Gil Balsiger <gil.balsiger@gmail.com>
2025-03-02 21:32:57 +0100

    refactor(combobox): rename css file, extract to stories

--- src/components/combobox/combobox.stories.ts
+++ src/components/combobox/combobox.stories.ts
@@ -18,81 +18,61 @@ type Story = StoryObj<ComboboxArgs>;
 export const Select: Story = {
   render: () => {
     return `
-<div>
-  <select name="fruits" id="combobox-1">
-    <option value="apple">Apple</option>
-    <option value="orange">Orange</option>
-    <option value="banana" selected>Banana</option>
-    <option value="grape">Grape</option>
-    <option value="kiwi">Kiwi</option>
-    <option value="mango">Mango</option>
-    <option value="strawberry">Strawberry</option>
-    <option value="watermelon">Watermelon</option>
-    <option value="pineapple">Pineapple</option>
-    <option value="pear">Pear</option>
-  </select>
-</div>
-<div>
-  <select name="fruits" id="combobox-2">
-    <option value="apple">Apple</option>
-    <option value="orange">Orange</option>
-    <option value="banana">Banana</option>
-    <option value="grape">Grape</option>
-    <option value="kiwi">Kiwi</option>
-    <option value="mango">Mango</option>
-    <option value="strawberry">Strawberry</option>
-    <option value="watermelon">Watermelon</option>
-    <option value="pineapple">Pineapple</option>
-    <option value="pear">Pear</option>
-  </select>
-</div>
-<form action="" method="get">
-  <select name="fruits" id="combobox-3" required multiple>
-    <option value="apple">Apple</option>
-    <option value="orange">Orange</option>
-    <option value="banana" selected>Banana</option>
-    <option value="grape">Grape</option>
-    <option value="kiwi">Kiwi</option>
-    <option value="mango">Mango</option>
-    <option value="strawberry" selected>Strawberry</option>
-    <option value="watermelon">Watermelon</option>
-    <option value="pineapple">Pineapple</option>
-    <option value="pear">Pear</option>
-  </select>
-</form>
+<select name="fruits" id="combobox-1">
+  <option value="apple">Apple</option>
+  <option value="orange">Orange</option>
+  <option value="banana" selected>Banana</option>
+  <option value="grape">Grape</option>
+  <option value="kiwi">Kiwi</option>
+  <option value="mango">Mango</option>
+  <option value="strawberry">Strawberry</option>
+  <option value="watermelon">Watermelon</option>
+  <option value="pineapple">Pineapple</option>
+  <option value="pear">Pear</option>
+</select>
 <script>
 new Combobox('#combobox-1');
+</script>`;
+  },
+};
+export const SelectSearchable: Story = {
+  render: () => {
+    return `
+<select name="fruits" id="combobox-2">
+  <option value="apple">Apple</option>
+  <option value="orange">Orange</option>
+  <option value="banana" selected>Banana</option>
+  <option value="grape">Grape</option>
+  <option value="kiwi">Kiwi</option>
+  <option value="mango">Mango</option>
+  <option value="strawberry">Strawberry</option>
+  <option value="watermelon">Watermelon</option>
+  <option value="pineapple">Pineapple</option>
+  <option value="pear">Pear</option>
+</select>
+<script>
 new Combobox('#combobox-2', { isSearchable: true });
-new Combobox('#combobox-3', { isSearchable: true });
+</script>`;
+  },
+};
 
-// Garbage collection example
-// let instance = new BloumSelect('#select-2', { isSearchable: true });
-// const weakRef = new WeakRef(instance);
-// const registry = new FinalizationRegistry((heldValue) => {
-//   console.log('Instance with id ' + heldValue +  ' has been garbage collected');
-// });
-//
-// registry.register(instance, 'BloumSelectInstance');
-//
-// // Destroy the instance
-// instance.destroy();
-//
-// // Force garbage collection (only works in environments where this is possible)
-// if (globalThis.gc) {
-//   globalThis.gc();
-// }
-//
-// // Check if the instance has been garbage collected
-// setTimeout(() => {
-//   if (weakRef.deref() === undefined) {
-//     console.log('Instance has been garbage collected');
-//   } else {
-//     console.log('Instance is still in memory');
-//     console.log(weakRef.deref());
-//   }
-// }, 3000);
-//
-// instance = null;
+export const SelectMultiple: Story = {
+  render: () => {
+    return `
+<select name="fruits" id="combobox-3" required multiple>
+  <option value="apple">Apple</option>
+  <option value="orange">Orange</option>
+  <option value="banana" selected>Banana</option>
+  <option value="grape">Grape</option>
+  <option value="kiwi">Kiwi</option>
+  <option value="mango">Mango</option>
+  <option value="strawberry" selected>Strawberry</option>
+  <option value="watermelon">Watermelon</option>
+  <option value="pineapple">Pineapple</option>
+  <option value="pear">Pear</option>
+</select>
+<script>
+new Combobox('#combobox-3', { isSearchable: true });
 </script>
     `;
   },
