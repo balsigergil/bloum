bf4ec05c728de17cf00f6a6965550fc87250e2b6
Gil Balsiger <gil.balsiger@gmail.com>
2025-03-07 22:32:01 +0100

    feat(breadcrumb): add breadcrumb component

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -31,7 +31,6 @@ const preview: Preview = {
       },
     },
   },
-  tags: ["autodocs"],
   decorators: [
     withThemeByClassName<HtmlRenderer>({
       themes: {
---
+++ src/components/breadcrumb/breadcrumb.stories.ts
@@ -0,0 +1,61 @@
+import type { Meta, StoryObj } from "@storybook/html";
+
+const meta: Meta = {
+  title: "Components/Breadcrumb",
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Breadcrumb: Story = {
+  render: () => {
+    return `<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="#">Home</a></li>
+    <li class="breadcrumb-item breadcrumb-separator"><i class="fas fa-angle-right"></i></li>
+    <li class="breadcrumb-item"><a href="#">Library</a></li>
+    <li class="breadcrumb-item breadcrumb-separator"><i class="fas fa-angle-right"></i></li>
+    <li class="breadcrumb-item active" aria-current="page">Data</li>
+  </ol>
+</nav>`;
+  },
+};
+
+export const BreadcrumbWithIcons: Story = {
+  render: () => {
+    return `<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i>Home</a></li>
+    <li class="breadcrumb-item breadcrumb-separator"><i class="fas fa-angle-right"></i></li>
+    <li class="breadcrumb-item"><a href="#"><i class="fas fa-book"></i>Library</a></li>
+    <li class="breadcrumb-item breadcrumb-separator"><i class="fas fa-angle-right"></i></li>
+    <li class="breadcrumb-item active" aria-current="page"><i class="fas fa-database"></i>Data</li>
+  </ol>
+</nav>`;
+  },
+};
+
+export const BreadcrumbWithMenu: Story = {
+  render: () => {
+    return `<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="#">Home</a></li>
+    <li class="breadcrumb-item breadcrumb-separator"><i class="fas fa-angle-right"></i></li>
+    <li class="breadcrumb-item">
+      <div class="dropdown">
+        <button data-menu="#menu" class="flex items-center gap-2">Library<i class="fas fa-angle-down text-xs"></i></button>
+        <div class="menu" id="menu">
+          <a href="#" class="menu-item">New File</a>
+          <a href="#" class="menu-item">Open File...</a>
+          <button class="menu-item">Save</button>
+          <div class="menu-divider"></div>
+          <button class="menu-item">Close</button>
+        </div>
+      </div>
+    </li>
+    <li class="breadcrumb-item breadcrumb-separator"><i class="fas fa-angle-right"></i></li>
+    <li class="breadcrumb-item active" aria-current="page"><i class="fas fa-database"></i>Data</li>
+  </ol>
+</nav>`;
+  },
+};
