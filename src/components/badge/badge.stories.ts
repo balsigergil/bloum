ed08a5e84347129895ec271b9c813e95cc6be5bf
Gil Balsiger <gil.balsiger@gmail.com>
2025-03-08 15:12:02 +0100

    feat(badge): add badge component

---
+++ src/components/badge/badge.stories.ts
@@ -0,0 +1,65 @@
+import type { Meta, StoryObj } from "@storybook/html";
+
+type BadgeArgs = {
+  text: string;
+  type: string;
+  topRight: boolean;
+  dot: boolean;
+  ping: boolean;
+};
+
+const meta: Meta<BadgeArgs> = {
+  title: "Components/Badge",
+  argTypes: {
+    text: {
+      control: "text",
+    },
+    type: {
+      control: "select",
+      options: ["default", "primary", "success", "danger", "warning", "info"],
+    },
+    topRight: {
+      control: "boolean",
+    },
+    dot: {
+      control: "boolean",
+    },
+    ping: {
+      control: "boolean",
+    },
+  },
+  args: {
+    text: "12",
+    type: "default",
+    topRight: false,
+    dot: false,
+    ping: false,
+  },
+  render: (args) => {
+    const badgeType = args.type === "default" ? "" : `badge-${args.type}`;
+    return `
+<button class="btn">
+  This is a button
+  <div class="badge ${badgeType}${args.topRight ? ` badge-top-right` : ``}${args.dot ? ` badge-dot` : ``}${args.ping ? ` badge-ping` : ``}">${!args.dot ? args.text : ``}</div>
+</button>`;
+  },
+};
+
+export default meta;
+type Story = StoryObj<BadgeArgs>;
+
+export const Badge: Story = {};
+
+export const BadgeTopRight: Story = {
+  args: {
+    topRight: true,
+  },
+};
+
+export const BadgeDot: Story = {
+  args: {
+    topRight: true,
+    dot: true,
+    ping: false,
+  },
+};
