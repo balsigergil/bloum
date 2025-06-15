e65fb5867e94bc63e4223e426bb466fadb37157e
Gil Balsiger <gil.balsiger@gmail.com>
2025-06-15 15:27:41 +0200

    refactor(toast): adjust styles for close button and description

--- src/components/toast/toast.ts
+++ src/components/toast/toast.ts
@@ -78,7 +78,7 @@ export class Toast {
     closeButton.className = "toast-close";
     closeButton.setAttribute("aria-label", "Close notification");
     closeButton.innerHTML = `
-      <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg width="10" height="10" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M13 1L1 13M1 1L13 13" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
       </svg>
     `;
