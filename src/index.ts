f07dd1c754b9609affffbc0015098433341b0acb
Gil Balsiger <gil.balsiger@gmail.com>
2025-09-06 18:54:00 +0200

    feat: make initialization functions globally available (fix #75)

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,32 @@
 import { init } from "./utils/init";
-import { AutogrowTextarea } from "./components/textarea/autogrow-textarea";
-import { Collapse } from "./components/collapse/collapse";
-import { ColorSchemeSwitcher } from "./components/color-scheme-switcher/color-scheme-switcher";
+import {
+  AutogrowTextarea,
+  initAutogrowTextarea,
+} from "./components/textarea/autogrow-textarea";
+import { Collapse, initCollapse } from "./components/collapse/collapse";
+import {
+  ColorSchemeSwitcher,
+  initColorSchemeSwitcher,
+} from "./components/color-scheme-switcher/color-scheme-switcher";
 import { Combobox } from "./components/combobox/combobox";
 import { CopyButton } from "./components/copy/copy";
 import { DataTable } from "./components/datatable/datatable";
-import { Drawer } from "./components/drawer/drawer";
-import { Menu } from "./components/menu/menu";
-import { Modal } from "./components/modal/modal";
-import { PinInput } from "./components/pin-input/pin-input";
-import { Popover } from "./components/popover/popover";
+import { Drawer, initDrawers } from "./components/drawer/drawer";
+import { initMenus, Menu } from "./components/menu/menu";
+import { initModals, Modal } from "./components/modal/modal";
+import { initPinInput, PinInput } from "./components/pin-input/pin-input";
+import { initPopover, Popover } from "./components/popover/popover";
 import { Tab } from "./components/tabs/tab";
 import { TabList } from "./components/tabs/tab-list";
 import { TabPanel } from "./components/tabs/tab-panel";
 import { Tabs } from "./components/tabs/tabs";
-import { Toast } from "./components/toast/toast";
-import { Tooltip } from "./components/tooltip/tooltip";
+import { initToast, Toast } from "./components/toast/toast";
+import { initTooltip, Tooltip } from "./components/tooltip/tooltip";
+import { initAccordion } from "./components/accordion/accordion";
+import { initSidebar } from "./components/sidebar/sidebar";
+import { initSteps } from "./components/steps/steps";
+import { initAvatar } from "./components/avatar/avatar";
+import { initPasswordToggle } from "./components/input/password-input";
 
 export {
   init,
@@ -36,4 +47,19 @@ export {
   Tabs,
   Toast,
   Tooltip,
+  initTooltip,
+  initSidebar,
+  initModals,
+  initDrawers,
+  initMenus,
+  initCollapse,
+  initAccordion,
+  initAutogrowTextarea,
+  initPopover,
+  initToast,
+  initColorSchemeSwitcher,
+  initSteps,
+  initAvatar,
+  initPasswordToggle,
+  initPinInput,
 };
