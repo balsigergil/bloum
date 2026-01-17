6eaaccb37d4a9bfad2f1cd4fe66eb215ff37cd16
Gil Balsiger <gil.balsiger@gmail.com>
2026-01-17 12:45:52 +0100

    refactor(cmd): rename classes

--- src/components/command-menu/command-menu.stories.ts
+++ src/components/command-menu/command-menu.stories.ts
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from "@storybook/html-vite";
 
 const meta: Meta = {
   title: "Components/Navigation/Command Menu",
+  tags: ["!autodocs"],
 };
 
 export default meta;
@@ -12,66 +13,33 @@ export const CommandMenu: Story = {
     return `
 <button class="btn" data-modal="#my-command-menu">Open command menu</button>
 <div class="modal" id="my-command-menu">
-  <div class="modal-content command-menu">
-    <div class="command-menu-header">
-      <i class="fas fa-magnifying-glass command-menu-search-icon"></i>
-      <input type="text" class="command-menu-input" placeholder="Search" />
+  <div class="modal-content cmd">
+    <div class="cmd-search">
+      <i class="fas fa-magnifying-glass"></i>
+      <input type="text" placeholder="Search" />
     </div>
-    <div class="command-menu-body">
-      <h3 class="command-menu-title">Home</h3>
-      <ul class="command-menu-list">
-        <li>
-          <a href="#" class="command-menu-item">
-            <div class="command-menu-link-label">
-              <i class="fas fa-home"></i> Dashboard
-            </div>
-            <div class="command-menu-link-description">Link</div>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="command-menu-item">
-            <div class="command-menu-link-label">
-              <i class="fas fa-bars-progress"></i> Projects
-            </div>
-            <div class="command-menu-link-description">Action</div>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="command-menu-item">
-            <div class="command-menu-link-label">
-              <i class="fas fa-cog"></i> Settings
-            </div>
-            <div class="command-menu-link-description">Link</div>
-          </a>
-        </li>
-      </ul>
-      <h3 class="command-menu-title">Home</h3>
-      <ul class="command-menu-list">
-        <li>
-          <a href="#" class="command-menu-item">
-            <div class="command-menu-link-label">
-              <i class="fas fa-home"></i> Dashboard
-            </div>
-            <div class="command-menu-link-description">Link</div>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="command-menu-item">
-            <div class="command-menu-link-label">
-              <i class="fas fa-bars-progress"></i> Projects
-            </div>
-            <div class="command-menu-link-description">Action</div>
-          </a>
-        </li>
-        <li>
-          <a href="#" class="command-menu-item">
-            <div class="command-menu-link-label">
-              <i class="fas fa-cog"></i> Settings
-            </div>
-            <div class="command-menu-link-description">Link</div>
-          </a>
-        </li>
-      </ul>
+    <div class="cmd-group">
+      <h3 class="cmd-group-title">Home</h3>
+      <div class="cmd-list">
+        <a href="#" class="cmd-item">
+          <div class="cmd-item-left">
+            <i class="fas fa-home"></i> Dashboard
+          </div>
+          <div class="cmd-item-right">Link</div>
+        </a>
+        <a href="#" class="cmd-item">
+          <div class="cmd-item-left">
+            <i class="fas fa-bars-progress"></i> Projects
+          </div>
+          <div class="cmd-item-right">Action</div>
+        </a>
+        <a href="#" class="cmd-item">
+          <div class="cmd-item-left">
+            <i class="fas fa-cog"></i> Settings
+          </div>
+          <div class="cmd-item-right">Link</div>
+        </a>
+      </div>
     </div>
   </div>
 </div>
