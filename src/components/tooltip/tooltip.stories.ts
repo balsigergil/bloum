e9f677d2bb4108113acb01b5c41f68618025f6b6
Gil Balsiger <gil.balsiger@gmail.com>
2026-05-25 14:47:31 +0200

    feat(tooltip): add max-width to tooltips

--- src/components/tooltip/tooltip.stories.ts
+++ src/components/tooltip/tooltip.stories.ts
@@ -19,3 +19,9 @@ export const Tooltip: Story = {
     text: "Hello World!",
   },
 };
+
+export const LongTooltip: Story = {
+  args: {
+    text: "This is a longer tooltip text to test the tooltip component's behavior with longer content. lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+};
